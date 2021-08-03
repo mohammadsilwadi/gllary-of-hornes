3fb84329ba5960f8fe0db89537d21901231a5af9
mohammadsilwadi <mohammadsilwadi1@gmail.com>
2021-08-03 14:04:15 +0300

    solve the lab02

--- src/components/ HornedBeast.js
+++ src/components/ HornedBeast.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react'
-
+import Col from 'react-bootstrap/Col';
+import { Card } from 'react-bootstrap';
 export class HornedBeast extends Component {
     constructor(){
         super()
@@ -19,13 +20,16 @@ export class HornedBeast extends Component {
         
         return (
             <div>
-                {console.log(this.props)}
-                <h2>{this.props.data.title}</h2>
-                <p>{this.props.data.description}</p>
+                <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                <Card.Title>{this.props.data.title}</Card.Title> 
+                <Card.Text> <p>{this.props.data.description}</p></Card.Text>
                 <img src={this.props.data.image_url} alt={this.props.title}style={{width:'200px'}}
                 onClick={this.addfav} /> 
                 <p> &#9825;
                 favorited={this.state.fav}</p>
+                </Card.Body>
+                </Card>
             </div>
             
         )
@@ -33,3 +37,10 @@ export class HornedBeast extends Component {
 }
 
 export default HornedBeast
+// {/* <Card.Body>
+// <Card.Title>{this.props.name}</Card.Title>
+// <Card.Text>
+// {this.props.title}
+// </Card.Text>
+// <Button variant="primary">{this.props.age}</Button>
+// </Card.Body> */}
